bb60e1bf2c37e684d97e02b30b29c68f0c118e29
Gaze | Mrp <108295652+quixote404@users.noreply.github.com>
2023-08-02 21:44:11 -0300

    "Sending the first file"

---
+++ Aula_01/helloworld.java
@@ -0,0 +1,23 @@
+// *------------------------------------------------------------------
+// Sobre a criação:
+// Autor : Matheus da Rosa - Quixotte
+// Repositório : https://github.com/quixote404
+// E-mail : hologramrp.@proton.me
+// *------------------------------------------------------------------
+// Sobre o programa:
+// Versão : 0.1 - DATA 15/01/2023
+// Data : Testes básicos efetuados abaixo
+// License : Null
+// *------------------------------------------------------------------
+
+package Aula_01;
+
+    public class helloworld {
+        public static void main(String[] args) {
+        InstrucaoImprimir();
+        }
+
+        static void InstrucaoImprimir() {
+            System.out.print(2 + 2);
+        }
+    }
